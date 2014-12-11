4be054811d578730c56a051171cb6c8818cbff29
Tiago Botelho <tiagonbotelho@hotmail.com>
2014-12-11 03:31:57 +0000

    Initialized repo

---
+++ .#Client.py
@@ -0,0 +1,1 @@
+tiagobotelho@MacBook-Pro-de-Tiago-2.local.1923
---
+++ Client.py
@@ -0,0 +1,13 @@
+# Echo client program
+import socket
+
+if __name__=='__main__':
+    HOST = '127.0.0.1'    # The remote host
+    PORT = 9000              # The same port as used by the server
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((HOST, PORT))
+    s.send(b'Hello, world\n')
+    s.send(b'te fudere')
+    data = s.recv(1024)
+    s.close()
+    print('Received', repr(data))
